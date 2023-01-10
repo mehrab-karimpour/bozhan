4dd08fcedd1485d327ecaf343478fbf1c328a030
mehrab.karimpour <mehrab.karimpour@elanza.com>
2023-01-10 16:25:49 +0330

    set dynamic root project

--- config/app.ts
+++ config/app.ts
@@ -1,14 +1,17 @@
 import 'dotenv'
 
+const appRoot = require('app-root-path').path;
+
+
 const appConfig = {
     appEnv: 'development',
     apiBase: process.env.HAS_API_BASE || false,
     port: process.env.APP_PORT || 5000,
     host: process.env.HOST || "localhost",
     secretKey: process.env.SECRET_KEY || 'bozhan',
-    appPath: '/home/oem/dev/production/bozhan',
-    sequelizeConfigDevelopment: '/home/oem/dev/production/bozhan/config/sequelize.ts',
-    sequelizeConfigProduction: '/home/oem/dev/production/bozhan/build/config/sequelize.js',
+    appPath: appRoot,
+    sequelizeConfigDevelopment: appRoot + '/config/sequelize.ts',
+    sequelizeConfigProduction: appRoot + '/build/config/sequelize.js',
 }
 
 export default appConfig
