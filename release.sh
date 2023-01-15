d66a8583b4239548479bc603cd64feb3f816f8c2
mehrab.karimpour <mehrab.karimpour@elanza.com>
2023-01-15 10:06:10 +0330

    refactor release.sh file when application is in the development mode

--- release.sh
+++ release.sh
@@ -1,25 +1,40 @@
-git reset --hard HEAD
-git clean -fd
-git pull
+filename='.env'
+mode="APP_ENV=production"
+read line <$filename
 
-pm2IsInstalled=$(npm ls -g | grep pm2)
-countOfPm2IsInstalledString=${#pm2IsInstalled}
-
-echo $countOfPm2IsInstalledString
-
-if [ 1 -ge $countOfPm2IsInstalledString ]; then
-  echo 'pm2 package not installed'
-  echo 'installing pm2...'
-  npm i -g pm2
+if [ $line != $mode ]; then
+  echo "---------------------------------------------------------"
+  echo "Application must be in production mode !"
+  echo "So please change the value of APP_ENV in .env file "
+  echo "APP_ENV=production"
+  echo "APP_ENV now is development......"
+  echo ""
 else
-  echo 'pm2 already is installed...'
+  echo "-- The application is in production mode --"
+  git reset --hard HEAD
+  git clean -fd
+  git pull
+  pm2IsInstalled=$(npm ls -g | grep pm2)
+  countOfPm2IsInstalledString=${#pm2IsInstalled}
+
+  echo $countOfPm2IsInstalledString
+
+  if [ 1 -ge $countOfPm2IsInstalledString ]; then
+    echo 'pm2 package not installed'
+    echo 'installing pm2...'
+    npm i -g pm2
+  else
+    echo 'pm2 already is installed...'
+  fi
+
+  pm2 stop server
+
+  rm -rf ./app/@types/*/
+  rm -rf ./build
+
+  npm run build
+
+  pm2 start build/server.js --watch
 fi
 
-pm2 stop server
 
-rm -rf ./app/@types/*/
-rm -rf ./build
-
-npm run build
-
-pm2 start build/server.js --watch
