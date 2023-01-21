7b21874115363baec3682e62c11d755ab8822402
mehrab.karimpour <mehrab.karimpour@elanza.com>
2023-01-21 21:49:54 +0330

    merge code serve application on local and docker container in release.sh

--- release.sh
+++ release.sh
@@ -19,25 +19,34 @@ else
     git pull
   fi
   npm i
-  pm2IsInstalled=$(npm ls -g | grep pm2)
-  countOfPm2IsInstalledString=${#pm2IsInstalled}
+  printf " Question-2 : do you want running app on local machine or docker container (l:local d:docker default:L) ? L/d : "
+  read answer
+  if [ "$answer" != "${answer#[Dd]}" ]; then
+    docker build -t bozhan .
+    docker run -dp 5000:5000 bozhan
 
-  echo $countOfPm2IsInstalledString
-
-  if [ 1 -ge $countOfPm2IsInstalledString ]; then
-    echo 'pm2 package not installed'
-    echo 'installing pm2...'
-    npm i -g pm2
   else
-    echo 'pm2 already is installed...'
+    pm2IsInstalled=$(npm ls -g | grep pm2)
+    countOfPm2IsInstalledString=${#pm2IsInstalled}
+
+    echo $countOfPm2IsInstalledString
+
+    if [ 1 -ge $countOfPm2IsInstalledString ]; then
+      echo 'pm2 package not installed'
+      echo 'installing pm2...'
+      npm i -g pm2
+    else
+      echo 'pm2 already is installed...'
+    fi
+
+    pm2 stop server
+
+    rm -rf ./app/@types/*/
+    rm -rf ./build
+
+    npm run build
+
+    pm2 start build/server.js --watch
   fi
 
-  pm2 stop server
-
-  rm -rf ./app/@types/*/
-  rm -rf ./build
-
-  npm run build
-
-  pm2 start build/server.js --watch
 fi
