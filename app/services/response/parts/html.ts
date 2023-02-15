463faf8afebb997c52e84a297555ae4cadd7d384
mehrab.karimpour <mehrab.karimpour@elanza.com>
2023-02-15 11:55:01 +0330

    using declare request instead of req  express object for to recognize custom properties in html

--- app/services/response/parts/html.ts
+++ app/services/response/parts/html.ts
@@ -1,5 +1,6 @@
 import ResponseAbstract from "app/services/response/responseAbstract";
 import {Request, Response} from "express";
+import {declareRequest} from "../../../@types";
 
 export default class Html extends ResponseAbstract {
 
@@ -10,7 +11,7 @@ export default class Html extends ResponseAbstract {
      * @param res
      * @param data
      */
-    public converter(req: Request, res: Response, data: any): any {
+    public converter(req: declareRequest, res: Response, data: any): any {
         return res.render(req.agentView, {data})
     }
 
